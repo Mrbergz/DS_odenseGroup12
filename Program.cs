1d27893ec5acae2f22c7673b5dc2733a5bdcbb76
linemoller <lmjo79125@edu.ucl.dk>
2026-02-12 10:27:48 +0100

    Opgave 3

--- Program.cs
+++ Program.cs
@@ -15,7 +15,28 @@
             Console.WriteLine("Hvilken geartype?");
             char geartype = Console.ReadLine()[0];
             Console.WriteLine($"{navn} har en {brand} {model} fra {år} som har geartype {geartype}!");
-         
+            
+            Console.Write("Hvor langt køre bilen på literen?");​
+            double kmPerLiter = Convert.ToDouble(Console.ReadLine());​​
+
+            Console.Write("Hvor lang er køreturen?");​
+            double distance = Convert.ToDouble(Console.ReadLine());​
+
+            Console.Write("Hvad er prisen pr. liter brændstof i DKK?");​
+            double fuelPrice = Convert.ToDouble(Console.ReadLine());​
+
+
+            double fuelNeeded = distance / kmPerLiter;​
+            double tripCost = fuelNeeded * fuelPrice;​
+
+​
+            Console.WriteLine($"Brændstofforbrug: {fuelNeeded.ToString("F2")} liter");​
+            Console.WriteLine($"Pris for køreturen: {tripCost.ToString("F2")} DKK");​
+            Console.WriteLine($"Køreturen på {distance} km koster {tripCost.ToString("F2")} DKK og bruger {fuelNeeded.ToString("F2")} liter brændstof.");​
+
+​
+
+            Console.ReadLine();
 
         }
     }
