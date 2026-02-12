0e02a2e9f5e68adb754b5f4884616933ecb8b645
linemoller <lmjo79125@edu.ucl.dk>
2026-02-12 10:29:37 +0100

    Lidt oprydning :)

---
+++ CarApp1/Program (2).cs
@@ -0,0 +1,43 @@
+﻿namespace CarProjekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hvad hedder du?");
+            string navn = Console.ReadLine();
+            Console.WriteLine("Skriv bilmærke");
+            string brand = Console.ReadLine();
+            Console.WriteLine("Skriv model");
+            string model = Console.ReadLine();
+            Console.WriteLine("Hvilken årgang?");
+            int år = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hvilken geartype?");
+            char geartype = Console.ReadLine()[0];
+            Console.WriteLine($"{navn} har en {brand} {model} fra {år} som har geartype {geartype}!");
+            
+            Console.Write("Hvor langt køre bilen på literen?");​
+            double kmPerLiter = Convert.ToDouble(Console.ReadLine());​​
+
+            Console.Write("Hvor lang er køreturen?");​
+            double distance = Convert.ToDouble(Console.ReadLine());​
+
+            Console.Write("Hvad er prisen pr. liter brændstof i DKK?");​
+            double fuelPrice = Convert.ToDouble(Console.ReadLine());​
+
+
+            double fuelNeeded = distance / kmPerLiter;​
+            double tripCost = fuelNeeded * fuelPrice;​
+
+​
+            Console.WriteLine($"Brændstofforbrug: {fuelNeeded.ToString("F2")} liter");​
+            Console.WriteLine($"Pris for køreturen: {tripCost.ToString("F2")} DKK");​
+            Console.WriteLine($"Køreturen på {distance} km koster {tripCost.ToString("F2")} DKK og bruger {fuelNeeded.ToString("F2")} liter brændstof.");​
+
+​
+
+            Console.ReadLine();
+
+        }
+    }
+}
