827647660cac6e798470ee8ab8e1b9d729bfa9d8
friisdev <philipfriis02@gmail.com>
2026-02-19 12:57:37 +0100

    TO DO
    Added TODO comments for project tasks and structure.

--- KontorNord - Projekt/program.cs
+++ KontorNord - Projekt/program.cs
@@ -1,1 +1,17 @@
+// TODO INDFOLDVERING
+// 1. Mødebooking
+// 2. Online Kalender
+// 3. Planlægning (Oversigt)
+// 4. Husk der er 3 mødelokaler.
 
+namespace KontorNord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+        }
+    }
+}
