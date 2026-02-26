95da357f89e7bd29556c47323cb591de139fdc72
friisdev <philipfriis02@gmail.com>
2026-02-26 13:10:57 +0100

    Done Primataltjekker

--- Primtaltjekker1/Program.cs
+++ Primtaltjekker1/Program.cs
@@ -1,32 +1,33 @@
-﻿class Program
+class Program
 {
-    static void Main(string[] args)
+    static void Main(string[] args) // PROGRAMMET STARTER
     {
-        Console.Write("Find alle primtal op til: ");
+        Console.Write("Skriv et tal for at finde alle primtal op til det: "); // VI ÅBNER MULIGHEDEN FOR IMPUT I CONSOLE
         int n = int.Parse(Console.ReadLine());
 
-        for (int i = 2; i <= n; i++)
+        for (int i = 2; i <= n; i++) // HER GENNEMGÅR VI ALLE TAL TIL N
         {
-            if (ErPrimtal(i))
+            if (ErPrimtal(i)) // KALDER FUNKTIONEN (ErPrimtal) - NEDENFOR
             {
-                Console.Write(i + " ");
+                Console.Write(i + " "); // HER PRINTER VI SVARET, HVIS DER ER PRIMTAL
             }
         }
     }
 
-    static bool ErPrimtal(int tal)
+    static bool ErPrimtal(int tal) // DETTE ER METODEN TIL AT FINDE PRIMTAL
     {
-        if (tal <= 1)
+        if (tal <= 1) // TJEKKER FOR LIGEGYLDIGE TAL, ALTSÅ UNDER 1
+
             return false;
 
-        int grænse = (int)Math.Sqrt(tal);
+        int grænse = (int)Math.Sqrt(tal); // HER BEREGNER VI KVADRATRODEN
 
-        for (int i = 2; i <= grænse; i++)
+        for (int i = 2; i <= grænse; i++) // HER DIVIDER VI
         {
             if (tal % i == 0)
                 return false;
         }
 
-        return true;
+        return true; // HVIS DIVISION VAR OKAY, SÅ SIGER VI OK
     }
 }
