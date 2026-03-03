7e6dd835aa37d3859024c609b20d64b108c1e3cb
linemoller <lmjo79125@edu.ucl.dk>
2026-03-03 11:32:15 +0100

    Tilføj CarClass

--- CarAppUge10/CarAppUge10/CarAppUge10/car.cs
+++ CarAppUge10/CarAppUge10/CarAppUge10/car.cs
@@ -1,1 +1,15 @@
 
+namespace CarAppUge10
+{
+    internal class Car
+    {
+        private string name;
+        private string brand;
+        private string model;
+        private int year;
+        private char gearType;
+        private double odometer;
+        private double kmPerLiter;
+        private bool isEngineOn;
+    }
+}
