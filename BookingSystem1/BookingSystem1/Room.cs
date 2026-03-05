0e1fa5926bf8ca5d0066fcf5ac2cbc3e14f8c8ac
friisdev <philipfriis02@gmail.com>
2026-03-05 13:52:20 +0100

    Tilføjer navn til lokale, samt liste til alle bookinger

--- BookingSystem1/BookingSystem1/Room.cs
+++ BookingSystem1/BookingSystem1/Room.cs
@@ -1,8 +1,10 @@
+using System.Collections.Generic;
+
 namespace MeetingBooking
 {
     internal class Room
     {
-      
+        private string name;
+        private List<Booking> _bookings = new List<Booking>();
     }
 }
-
