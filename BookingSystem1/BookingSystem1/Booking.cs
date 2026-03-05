35f2c5fe7c88a5a4c7f0489023bf961ed7bcd34c
friisdev <philipfriis02@gmail.com>
2026-03-05 13:58:18 +0100

    Tilføjer felter og properties. Reference, titel på mødet, start- og sluttid og dato

--- BookingSystem1/BookingSystem1/Booking.cs
+++ BookingSystem1/BookingSystem1/Booking.cs
@@ -1,7 +1,14 @@
+using System;
+
 namespace MeetingBooking
 {
     internal class Booking
     {
-      
+        private Room _room;
+
+        public string Title { get; }
+        public DateTime StartTime { get; }
+        public DateTime EndTime { get; }
+        public DateTime BookingDate { get; }
     }
 }
