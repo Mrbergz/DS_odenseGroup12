9b824d38a8f2d4d5ff55e765092dba4748b13123
friisdev <philipfriis02@gmail.com>
2026-03-05 14:13:22 +0100

    Tilføjer "main class" og "namespace"

--- BookingSystem1/BookingSystem1/Program.cs
+++ BookingSystem1/BookingSystem1/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace BookingSystem1
+using System;
+using System.Collections.Generic;
+using MeetingBooking;
+
+namespace MeetingBookingApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
