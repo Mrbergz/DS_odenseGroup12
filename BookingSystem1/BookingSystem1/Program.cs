3cd1dac53072f4f79130f463e5359ce97b50ab2c
linemoller <lmjo79125@edu.ucl.dk>
2026-03-05 13:45:26 +0100

    mappe

---
+++ BookingSystem1/BookingSystem1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BookingSystem1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
