06f9a301a5841f4d4e0364748c47e9601f5eb859
friisdev <philipfriis02@gmail.com>
2026-03-05 13:10:18 +0100

    Refactor Car class properties and methods

--- CarAppUge10/CarAppUge10/CarAppUge10/car.cs
+++ CarAppUge10/CarAppUge10/CarAppUge10/car.cs
@@ -1,8 +1,11 @@
+using System;
+using System.Collections.Generic;
+
 namespace CarAppUge10
 {
     internal class Car
     {
-        // PRIVATE FIELDS
+        // PRIVATE ATTRIBUTTER
         private string brand;
         private string model;
         private int year;
@@ -12,50 +15,17 @@ namespace CarAppUge10
         private double kmPerLiter;
         private double lastTripPrice;
 
+        private List<Trip> _trips = new List<Trip>();
+
         // PROPERTIES
-        public string Brand
-        {
-            get => brand;
-            set => brand = value;
-        }
-
-        public string Model
-        {
-            get => model;
-            set => model = value;
-        }
-
-        public int Year
-        {
-            get => year;
-            set => year = value;
-        }
-
-        public char Gear
-        {
-            get => gear;
-            set => gear = value;
-        }
-
-        public double Odometer
-        {
-            get => odometer;
-            set => odometer = value;
-        }
-
+        public string Brand { get => brand; set => brand = value; }
+        public string Model { get => model; set => model = value; }
+        public int Year { get => year; set => year = value; }
+        public char Gear { get => gear; set => gear = value; }
+        public double Odometer { get => odometer; set => odometer = value; }
         public FuelType FuelType { get; private set; }
-
-        public double KmPerLiter
-        {
-            get => kmPerLiter;
-            set => kmPerLiter = value;
-        }
-
-        public bool IsEngineOn
-        {
-            get => isEngineOn;
-            set => isEngineOn = value;
-        }
+        public double KmPerLiter { get => kmPerLiter; set => kmPerLiter = value; }
+        public bool IsEngineOn { get => isEngineOn; set => isEngineOn = value; }
 
         // KONSTRUKTØR
         public Car(string brand, string model, int year, char gear, string fuelType, double kmPerLiter)
@@ -68,33 +38,42 @@ namespace CarAppUge10
             this.odometer = 0;
             this.isEngineOn = false;
 
-            // Konverter string til enum
             FuelType = Enum.Parse<FuelType>(fuelType, true);
         }
 
         // METODER
-        public void StartEngine()
-        {
-            isEngineOn = true;
-        }
-
-        public void StopEngine()
-        {
-            isEngineOn = false;
-        }
+        public void StartEngine() => isEngineOn = true;
+        public void StopEngine() => isEngineOn = false;
 
         public void Drive(double distance)
         {
-            if (isEngineOn)
-            {
-                odometer += distance;
-                Console.WriteLine($"Bilen kørte {distance} km.");
-                Console.WriteLine($"Ny kilometerstand: {odometer} km.");
-            }
-            else
+            if (!isEngineOn)
             {
                 Console.WriteLine("Motoren er slukket. Start motoren før kørsel.");
+                return;
             }
+
+            odometer += distance;
+            Console.WriteLine($"Bilen kørte {distance} km.");
+            Console.WriteLine($"Ny kilometerstand: {odometer} km.");
+        }
+
+        public void Drive(Trip newTrip)
+        {
+            if (!isEngineOn)
+            {
+                Console.WriteLine("Motoren er slukket. Start motoren før kørsel.");
+                return;
+            }
+
+            // Opdater odometer
+            odometer += newTrip.Distance;
+
+            // Gem turen
+            _trips.Add(newTrip);
+
+            Console.WriteLine($"Turen på {newTrip.Distance} km er registreret.");
+            Console.WriteLine($"Ny kilometerstand: {odometer} km.");
         }
 
         public double CalculateTripPrice(double distance, double literPrice, string fuelType)
@@ -128,6 +107,11 @@ namespace CarAppUge10
                 $"Kilometerstand: {odometer} km\n" +
                 $"Km pr. liter: {kmPerLiter}\n" +
                 $"Sidste turpris: {lastTripPrice:F2} DKK";
+        }
+
+        public List<Trip> GetTrips()
+        {
+            return _trips;
         }
     }
 }
