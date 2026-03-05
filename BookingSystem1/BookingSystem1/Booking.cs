7b4d67d51863295587d5d8da0e0adaa3b7dc3be0
friisdev <philipfriis02@gmail.com>
2026-03-05 14:00:29 +0100

    Tilføjer konstruktøren + Vi sætter bookingdate til startTime.Date for at optimere.

--- BookingSystem1/BookingSystem1/Booking.cs
+++ BookingSystem1/BookingSystem1/Booking.cs
@@ -10,5 +10,15 @@ namespace MeetingBooking
         public DateTime StartTime { get; }
         public DateTime EndTime { get; }
         public DateTime BookingDate { get; }
+
+        public Booking(Room room, string title, DateTime startTime, DateTime endTime)
+        {
+            _room = room;
+            Title = title;
+            StartTime = startTime;
+            EndTime = endTime;
+            BookingDate = startTime.Date;
+        }
+
     }
 }
